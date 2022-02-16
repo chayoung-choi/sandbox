4ca18340775f8c25a510cefbaf68a9620e5dfee5
cyoung90 <chayoung.dev@gmail.com>
2022-02-16 22:45:23 +0900

    220216. Layout

--- src/main/java/com/eden/sandbox/pdf/PdfController.java
+++ src/main/java/com/eden/sandbox/pdf/PdfController.java
@@ -1,43 +1,28 @@
 package com.eden.sandbox.pdf;
 
-import com.eden.sandbox.aop.LogExecutionTime;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.File;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
-@Api(tags = {"PDF 관련 기능 Controller"})
-@RestController
 @RequestMapping("/pdf")
-@Log4j2
+@Controller
 public class PdfController {
 
-  @ApiOperation(value = "PDF 정보 가져오기")
-//  @ApiImplicitParam(name = "tagIds", value = "검색할 태그 ID를 담은 리스트", dataType = "list")
-  @GetMapping("/info")
-  @LogExecutionTime
-  public String getPdfInfo(){
+  private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
 
-    log.info("pdf");
-    File file = new File("pdf파일의위치");
-//    PDDocument document = PDDocument.load(file);
-//    PDFRenderer pdfRenderer = new PDFRenderer(pdfDoc);
+  @GetMapping("")
+  public String main(Model model) {
 
-    return "pdf 파일";
+//    ModelMap model = new ModelMap();
+    model.addAttribute("message", "message");
+    model.addAttribute("tasks", tasks);
+    return "pdf/index";
   }
 
-  @ApiOperation(value = "조건에 맞는 게시글 목록을 반환하는 메소드")
-  @ApiImplicitParam(name = "tagIds", value = "검색할 태그 ID를 담은 리스트", dataType = "list")
-  @GetMapping("/convert/{fileName}")
-  public String convertPdf(@PathVariable String fileName){
-
-    //    File file = new File("pdf파일의위치");
-//    PDDocument document = PDDocument.load(file);
-//    PDFRenderer pdfRenderer = new PDFRenderer(pdfDoc);
-
-    return "Response!!! >> " + fileName;
-  }
 }
